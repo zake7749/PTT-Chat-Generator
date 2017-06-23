9a7b053941a61252eeab68661dcff662493ca196
zake7749 <zake7749@gmail.com>
2017-06-23 23:11:30 +0800

    Clean the deprecated modules.

--- Matcher/sentenceFilter.py
+++
@@ -1,8 +0,0 @@
-
-class SentenceFilter(object):
-    """
-    從所有 query 中，排除掉與使用者意圖完全無關的 query
-    """
-
-    #TODO
-    pass
--- Matcher/wordBagMatcher.py
+++
@@ -1,6 +0,0 @@
-from .matcher import Matcher
-
-# 此匹配模組已優化為 WordWeightMatcher
-
-class WordBagMatcher(Matcher):
-    pass
